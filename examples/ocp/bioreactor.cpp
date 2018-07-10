c5df5bc2cb5e90d361a03ddd5051333035ea504d
HP Boaz <boazfloor@gmail.com>
2018-07-10 16:52:52 +0200

    Added slack variable to collision constraints

--- examples/ocp/bioreactor.cpp
+++ examples/ocp/bioreactor.cpp
@@ -41,8 +41,8 @@ int main( ){
 
     // INTRODUCE THE VARIABLES:
     // -------------------------
-    DifferentialState     x,y,theta;
-    Control               v,w   ;
+    DifferentialState     x,y,theta, dummy;
+    Control               v,w, sv;
     DifferentialEquation  f    ;
 
     OnlineData goal_x;
@@ -59,6 +59,8 @@ int main( ){
     OnlineData wY_T;
     OnlineData wTheta_T;
 
+    OnlineData ws;
+
     OnlineData r_disc;
     OnlineData disc_pos;
 
@@ -74,18 +76,19 @@ int main( ){
     f << dot(x) == v*cos(theta);
     f << dot(y) == v*sin(theta);
     f << dot(theta) == w;
+    f << dot(dummy) == sv;
 
     // DEFINE AN OPTIMAL CONTROL PROBLEM:
     // ----------------------------------
-    OCP ocp( 0.0, 5.0, 50.0 );
+    OCP ocp( 0.0, 2.5, 50.0 );
 
-    ocp.setNOD(18);
+    ocp.setNOD(19);
 
-    ocp.minimizeLagrangeTerm(wX*(x-goal_x)*(x-goal_x)+ wY*(y-goal_y)*(y-goal_y)+ wTheta*(theta-goal_theta)*(theta-goal_theta)+wV*v*v+wW*w*w );  // weigh this with the physical cost!!!
+    ocp.minimizeLagrangeTerm(wX*(x-goal_x)*(x-goal_x)+ wY*(y-goal_y)*(y-goal_y)+ wTheta*(theta-goal_theta)*(theta-goal_theta)+wV*v*v+wW*w*w + ws*sv*sv );  // weigh this with the physical cost!!!
 
     ocp.minimizeMayerTerm(wX_T*(x-goal_x)*(x-goal_x)+ wY_T*(y-goal_y)*(y-goal_y)+ wTheta_T*(theta-goal_theta)*(theta-goal_theta));
     
-ocp.subjectTo( f );
+    ocp.subjectTo( f );
 
     ocp.subjectTo( -1.0 <= v <= 1.0 );
     ocp.subjectTo( -1.0 <= w <= 1.0 );
@@ -106,28 +109,31 @@ ocp.subjectTo( f );
     R_obst(1,1) = cos(obst1_theta);
 
     Expression deltaPos_disc1(2,1);
-    deltaPos_disc1(0) =  x - cos(obst1_theta)*disc_pos - obst1_x;
-    deltaPos_disc1(1) =  y - sin(obst1_theta)*disc_pos - obst1_y;
+    deltaPos_disc1(0) =  x - obst1_x;// - cos(obst1_theta)*disc_pos;
+    deltaPos_disc1(1) =  y - obst1_y;// - sin(obst1_theta)*disc_pos;
 
-    Expression deltaPos_disc2(2,1);
-    deltaPos_disc2(0) = x + cos(obst1_theta)*disc_pos - obst1_x;
-    deltaPos_disc2(1) = y + sin(obst1_theta)*disc_pos - obst1_y;
+//    Expression deltaPos_disc2(2,1);
+//    deltaPos_disc2(0) = x - obst1_x + cos(obst1_theta)*disc_pos;
+//    deltaPos_disc2(1) = y - obst1_y + sin(obst1_theta)*disc_pos;
 
     Expression c_obst1_1, c_obst1_2;
     c_obst1_1 = deltaPos_disc1.transpose() * R_obst.transpose() * ab * R_obst * deltaPos_disc1;
-    c_obst1_2 = deltaPos_disc2.transpose() * R_obst.transpose() * ab * R_obst * deltaPos_disc2;
+//    c_obst1_2 = deltaPos_disc2.transpose() * R_obst.transpose() * ab * R_obst * deltaPos_disc2;
 
-    ocp.subjectTo(c_obst1_1 >= 1);
-    ocp.subjectTo(c_obst1_2 >= 1);
+//    c_obst1_1 = ((cos(obst1_theta)*(cos(obst1_theta)*(obst1_x - x + disc_pos*cos(obst1_theta)) - sin(obst1_theta)*(obst1_y - y + disc_pos*sin(obst1_theta))))/((obst1_major + r_disc)*(obst1_major + r_disc)) + (sin(obst1_theta)*(sin(obst1_theta)*(obst1_x - x + disc_pos*cos(obst1_theta)) + cos(obst1_theta)*(obst1_y - y + disc_pos*sin(obst1_theta))))/((obst1_minor + r_disc)*(obst1_minor + r_disc)))*(obst1_x - x + disc_pos*cos(obst1_theta)) + ((cos(obst1_theta)*(sin(obst1_theta)*(obst1_x - x + disc_pos*cos(obst1_theta)) + cos(obst1_theta)*(obst1_y - y + disc_pos*sin(obst1_theta))))/((obst1_minor + r_disc)*(obst1_minor + r_disc)) - (sin(obst1_theta)*(cos(obst1_theta)*(obst1_x - x + disc_pos*cos(obst1_theta)) - sin(obst1_theta)*(obst1_y - y + disc_pos*sin(obst1_theta))))/((obst1_major + r_disc)*(obst1_major + r_disc)))*(obst1_y - y + disc_pos*sin(obst1_theta));
+
+    ocp.subjectTo(c_obst1_1 - sv >= 1);
+//    ocp.subjectTo(c_obst1_2 >= 1);
 
 	// DEFINE AN MPC EXPORT MODULE AND GENERATE THE CODE:
 	// ----------------------------------------------------------
 	OCPexport mpc( ocp );
 
 	mpc.set( HESSIAN_APPROXIMATION,       EXACT_HESSIAN  		);
-	mpc.set( DISCRETIZATION_TYPE,         MULTIPLE_SHOOTING 	);
-	mpc.set( INTEGRATOR_TYPE,             INT_RK4			);
-	mpc.set( NUM_INTEGRATOR_STEPS,        20            		);
+    mpc.set( DISCRETIZATION_TYPE,         MULTIPLE_SHOOTING 	);
+	mpc.set( INTEGRATOR_TYPE,             INT_RK4			    );
+//    mpc.set( INTEGRATOR_TYPE,             INT_IRK_GL4			);
+	mpc.set( NUM_INTEGRATOR_STEPS,        50            		);
 	mpc.set( QP_SOLVER,                   QP_QPOASES    		);
 	mpc.set( HOTSTART_QP,                 NO             		);
 	mpc.set( GENERATE_TEST_FILE,          YES            		);
