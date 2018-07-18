3c02cc7429dbb6e4deca5fa8a2c2aea4265e51de
Bruno Brito <bruno.debrito@tudelft.nl>
2018-07-18 11:18:32 +0200

    first mpcc implementation working

--- examples/ocp/bioreactor.cpp
+++ examples/ocp/bioreactor.cpp
@@ -84,7 +84,7 @@ int main( ){
     f << dot(x) == v*cos(theta);
     f << dot(y) == v*sin(theta);
     f << dot(theta) == w;
-	f << dot(s) == v;
+	f << dot(s) == v*0.4;
 
     // DEFINE AN OPTIMAL CONTROL PROBLEM:
     // ----------------------------------
@@ -98,7 +98,7 @@ int main( ){
 	Expression error_lag       = -dx_path_norm * (x - x_path) - dy_path_norm * (y - y_path);
 
 
-	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour + Wy*error_lag*error_lag + Ww*w*w +Wv*(v-1)*(v-1));// weight this with the physical cost!!!
+	ocp.minimizeLagrangeTerm(Wx*error_contour*error_contour + Wy*error_lag*error_lag + Ww*w*w +Wv*(v-0.5)*(v-0.5));// weight this with the physical cost!!!
     //ocp.subjectTo( f );
 	ocp.setModel(f);
 
